0331f670303f97809d91a0e707c4b42ec7579047
RawanAlali <rawanali95t@gmail.com>
2023-05-01 12:34:16 +0300

    all files

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+// let campus = document.querySelectorAll("#campus .father .im")
+// let div=document.querySelectorAll("#campus .father div div")
+// console.log(campus)
+// console.log(div)
+// campus.forEach(el => {
+//     el.addEventListener('mouseenter', () => {
+//         console.log(el)
+//         div.innerText= `}+pp`;
+      
+
+//     })
+// });
+// // // campus[0].style.b
+// for (let i = 0; i < campus.length; i++){
+//     campus[i].addEventListener('mouseenter',()=>{
+//         console.log(div)
+//         div.innerText="["
+//     })
+// }
